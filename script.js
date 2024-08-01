cfe784b07dfceff4f67ed7c5c6742fa874dab326
Jeniffer <jeniteixeira@outlook.com.br>
2024-07-31 22:50:55 -0300

    1° commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function gerarnumero() {
+
+    const min = Math.ceil(document.querySelector(".min").value)
+    const max = Math.floor(document.querySelector(".max").value)
+
+    const resultado = Math.floor(Math.random() * (max-min+1)) + min;
+
+alert(resultado)
+}
